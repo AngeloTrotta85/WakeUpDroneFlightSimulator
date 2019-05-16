24ea8c4dbe16ab67c62b06339027d6b13c4d1ca3
Angelo <angelo@magilia.com>
2019-05-16 11:09:28 +0200

    Initial commit

---
+++ src/WakeUpDroneFlightSimulator.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : WakeUpDroneFlightSimulator.cpp
+// Author      : Angelo Trotta
+// Version     :
+// Copyright   :  GNU GENERAL PUBLIC LICENSE - Version 3, 29 June 2007
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
